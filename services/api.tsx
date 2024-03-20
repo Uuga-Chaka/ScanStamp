200d9bc766a3815db022ca19338794da904a0e1b
Juan Gallego <jpgarias09@gmail.com>
2024-03-20 02:38:41 -0500

    refactor: :art: Change return type of get on api
    The function get now returns a especific object of val() instead of the while firebase db snapshot
    
    It's clearer for the developer to define what is going to be the return type of the resolved promise

--- services/api.tsx
+++ services/api.tsx
@@ -2,7 +2,10 @@ import database from '@react-native-firebase/database'
 
 const dbRef = database().ref('/api/')
 
-const get = async (url: string) => await dbRef.child(url).once('value')
+const get = async <T,>(url: string): Promise<T> => {
+  const snapshot = await dbRef.child(url).once('value')
+  return snapshot.val() as T
+}
 const post = async <T,>(url: string, value: T) => await dbRef.child(url).set(value)
 const remove = async (url: string) => await dbRef.child(url).remove()
 const update = async <T,>(url: string, value: { [key: string]: T }) =>
