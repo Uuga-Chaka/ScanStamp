f1c65be1ef9f148d696b98949d1c1bd831dd55f9
Juan Gallego <jpgarias09@gmail.com>
2024-03-24 20:23:53 -0500

    chore: :globe_with_meridians: Adde language
    Spanish added as a language, a js object has been created
    
    Spanish

---
+++ localization/ES.js
@@ -0,0 +1,42 @@
+module.exports = {
+  common: {
+    signUp: 'Registrarse',
+    logIn: 'Iniciar sesión',
+  },
+  buttons: {
+    done: 'Listo',
+    submit: 'Enviar',
+    yes: 'Sí',
+    no: 'No',
+    addPermission: 'Agregar permiso',
+    scanQR: 'Escanear código QR',
+    signOut: 'Cerrar sesión',
+  },
+  errMsg: {
+    getUserInfo: 'Hubo un error al intentar obtener la información del usuario',
+    requiredEmail: 'Se requiere un correo electrónico',
+    passwordRequired: 'Se requiere una contraseña',
+    invalidEmail: 'El correo electrónico no es válido',
+    passwordShort: 'La contraseña es demasiado corta',
+    noSamePassword: 'Las contraseñas no son iguales',
+    noSameEmail: 'Los correos electrónicos no son iguales',
+  },
+  cameraDialog: {
+    exist: 'Existe',
+    userAdded: 'Esto ya existe',
+    addUser: 'Agregar usuario',
+    verifyAddUsers: '¿Desea agregar a estos usuarios a la base de datos?',
+  },
+  input: {
+    email: 'Correo electrónico',
+    password: 'Contraseña',
+    validateEmail: 'Validar correo electrónico',
+    validatePassword: 'Validar contraseña',
+  },
+  textNavigation: {
+    accept: 'Acepto los',
+    terms: 'Términos y Condiciones.',
+    alreadyHaveAccount: '¿Ya tienes una cuenta?',
+    dontHaveAccount: '¿No tienes una cuenta?',
+  },
+}
