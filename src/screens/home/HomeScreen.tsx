d7d14f952cf1f529ac4ffd84865b223a52eb59e5
Juan Gallego <jpgarias09@gmail.com>
2024-03-30 08:34:29 -0500

    feat: :art: Add icons
    Icones were added to the bottom tab navigation
    
    I was not possible to identify the tab type

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,31 +1,48 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
-import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { useCameraPermission } from 'react-native-vision-camera'
+import { Icon } from 'react-native-paper'
 
 import { CameraContainer } from '../camera/CameraScreen'
 import { Profile } from '../profile/Profile'
 import { QRList } from '../qrList/QRList'
 import { QRScan } from '../qrScan/QRScan'
-import { RoutesTypes, TabParamList } from '../../routes/Routes'
+import { TabParamList } from '../../routes/Routes'
 
 const Tab = createMaterialBottomTabNavigator<TabParamList>()
-type HomeScreenProps = NativeStackScreenProps<RoutesTypes, 'HomeScreen'>
 
-export const HomeScreen = ({ navigation }: HomeScreenProps) => {
-  const { hasPermission } = useCameraPermission()
-
-  useEffect(() => {
-    !hasPermission && navigation.navigate('PermissionScreen')
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
+export const HomeScreen = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='QRScanScreen' component={CameraContainer} />
-      <Tab.Screen name='QRCodeScreen' component={QRScan} />
-      <Tab.Screen name='QRListScreen' component={QRList} />
-      <Tab.Screen name='ProfileScreen' component={Profile} />
+    <Tab.Navigator labeled={false}>
+      <Tab.Screen
+        name='QRScanScreen'
+        component={CameraContainer}
+        options={{
+          tabBarIcon: ({ color }) => <Icon source={'camera'} color={color} size={24} />,
+        }}
+      />
+      <Tab.Screen
+        name='QRCodeScreen'
+        component={QRScan}
+        options={{
+          tabBarIcon: ({ color }) => <Icon source={'qrcode'} color={color} size={24} />,
+        }}
+      />
+      <Tab.Screen
+        name='QRListScreen'
+        component={QRList}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon source={'format-list-bulleted'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='ProfileScreen'
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => <Icon source={'account-outline'} color={color} size={24} />,
+        }}
+      />
     </Tab.Navigator>
   )
 }
